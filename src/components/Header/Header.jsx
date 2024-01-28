6b42ceec5bda4cb8051528c6cb3d3870971aa14e
Tushar Sharma <tush.sharma2504@gmail.com>
2024-01-28 21:57:33 +0530

    Added Map and Modified Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,67 +1,79 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { alpha, styled } from '@mui/material/styles'; 
-import { createTheme } from '@mui/material';
-import { CenterFocusStrong } from "@mui/icons-material";
+import { alpha, styled } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
 
 const theme = createTheme();
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  marginRight: theme.spacing(2), 
+  marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
 
 const Header = () => {
   return (
-    <AppBar position="static" elevation={3}>
+    <AppBar
+      position="sticky"
+      elevation={5}
+      sx={{ backgroundColor: "#194F92", color: "#CBD5F0" }}
+    >
       <Toolbar>
         <Typography variant="h4" sx={{ flexGrow: 1 }}>
           Travezy
         </Typography>
-        <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <Typography variant="body1" sx={{ marginRight: '5px' }}>
-                Explore new horizons
-            </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="body1" sx={{ marginRight: "5px" }}>
+            Explore new horizons
+          </Typography>
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
-            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
           </Search>
         </Box>
       </Toolbar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 
-const Map = () => {
+import MapGL from 'react-map-gl';
+import React from "react";
+
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import { LocationOnOutlined } from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/material/Rating";
+import { createTheme } from "@mui/material";
+import { alpha, styled } from "@mui/material/styles"
+
+
+const theme = createTheme();
+
+const mapContainer = styled("div")(({ theme }) => ({
+  height: "85vh",
+  width: "100%",
+}));
+
+
+const Map = ()=> {
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 28.704060, lng: 77.102493 };
   return (
-    <div>
-      <h1>Map</h1>
-    </div>
-  )
+    <>
+    <mapContainer>
+    <MapGL
+      mapboxAccessToken="pk.eyJ1IjoidGhldHVzaGFyc2hhcm1hIiwiYSI6ImNscnhlbm81ZTB4MHoyam14bmtldGZhb2IifQ.hXsEzi-gJBIx-8-PUTz8Pg"
+      initialViewState={{
+        longitude: coordinates.lng,
+        latitude: coordinates.lat,
+        zoom: 14
+      }}
+      style={{ width: "100%", height: "90vh" }}      mapStyle="mapbox://styles/thetusharsharma/clrxfygnv009301ph879uha27"
+      
+    />
+    </mapContainer>
+    </>
+  );
 }
 export default Map;
